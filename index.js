7ffb5cf18694ecbf31b8f9665b4f42eefe2a2ae4
Akash Vaghela <akashvaghela09@gmail.com>
2022-01-25 21:39:32 +0530

    file updated for cors error

--- index.js
+++ index.js
@@ -6,6 +6,13 @@ require("dotenv").config();
 const PORT = 2244
 
 app.use(express.json())
+app.use(cors());
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
 // connect express.js with mongoDB
 const connect = async () => {
